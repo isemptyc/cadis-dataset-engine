71901e8cac8d456a6e8da355860190ab794bdaeb
Isempty <isempty.chiu@gmail.com>
2026-05-12 02:33:24 +0800

    feat(gq): add Equatorial Guinea admin engine

--- build.py
+++ build.py
@@ -13,6 +13,7 @@ from engines.am.engine_am import ArmeniaAdminEngine
 from engines.ar.engine_ar import ArgentinaAdminEngine
 from engines.au.engine_au import AustraliaAdminEngine
 from engines.az.engine_az import AzerbaijanAdminEngine
+from engines.gq.engine_gq import EquatorialGuineaAdminEngine
 from engines.eg.engine_eg import EgyptAdminEngine
 from engines.dj.engine_dj import DjiboutiAdminEngine
 from engines.cd.engine_cd import DemocraticRepublicoftheCongoAdminEngine
@@ -1837,6 +1838,19 @@ def main() -> int:
 
     if country == "eg":
         EgyptAdminEngine.prepare_datasets(
+            osm_pbf_path=args.osm,
+            work_dir=work_dir,
+            country_geometry_path=args.country_geometry,
+        )
+        _write_source_osm_identity(
+            work_dir=work_dir,
+            osm_pbf_path=args.osm,
+        )
+        print(work_dir)
+        return 0
+
+    if country == "gq":
+        EquatorialGuineaAdminEngine.prepare_datasets(
             osm_pbf_path=args.osm,
             work_dir=work_dir,
             country_geometry_path=args.country_geometry,
---
+++ engines/gq/__init__.py
@@ -0,0 +1,3 @@
+from .engine_gq import EquatorialGuineaAdminEngine
+
+__all__ = ["EquatorialGuineaAdminEngine"]
