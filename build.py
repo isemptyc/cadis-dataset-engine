c97e108cee033312ffd29c95a353d5b34f4ecc08
Isempty <isempty.chiu@gmail.com>
2026-05-12 03:04:55 +0800

    feat(sl): add Sierra Leone admin engine

--- build.py
+++ build.py
@@ -13,6 +13,7 @@ from engines.am.engine_am import ArmeniaAdminEngine
 from engines.ar.engine_ar import ArgentinaAdminEngine
 from engines.au.engine_au import AustraliaAdminEngine
 from engines.az.engine_az import AzerbaijanAdminEngine
+from engines.sl.engine_sl import SierraLeoneAdminEngine
 from engines.sc.engine_sc import SeychellesAdminEngine
 from engines.gm.engine_gm import GambiaAdminEngine
 from engines.sn.engine_sn import SenegalAdminEngine
@@ -2229,6 +2230,19 @@ def main() -> int:
 
     if country == "sc":
         SeychellesAdminEngine.prepare_datasets(
+            osm_pbf_path=args.osm,
+            work_dir=work_dir,
+            country_geometry_path=args.country_geometry,
+        )
+        _write_source_osm_identity(
+            work_dir=work_dir,
+            osm_pbf_path=args.osm,
+        )
+        print(work_dir)
+        return 0
+
+    if country == "sl":
+        SierraLeoneAdminEngine.prepare_datasets(
             osm_pbf_path=args.osm,
             work_dir=work_dir,
             country_geometry_path=args.country_geometry,
---
+++ engines/sl/__init__.py
@@ -0,0 +1,3 @@
+from .engine_sl import SierraLeoneAdminEngine
+
+__all__ = ["SierraLeoneAdminEngine"]
